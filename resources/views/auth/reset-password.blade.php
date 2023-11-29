410be83bf3c3942b946e4de33d88f8e68f4c8838
Femscode <fasanyafemi@gmail.com>
2023-11-29 02:50:53 +0100

    sm_new

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,175 +1,65 @@
-<!DOCTYPE html>
+@extends('layouts.app')
 
-<html lang="en">
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Reset Password') }}</div>
 
-<head>
-    <title>VTUBIZ | Reset Password</title>
-    <meta charset="utf-8" />
-    <meta name="description" content="Top Up, Pay Bills, Stay Connected" />
-    <meta name="keywords" content="CTtaste" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta property="og:locale" content="en_US" />
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content="CTTaste" />
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+                <div class="card-body">
+                    <form method="POST" action="{{ route('password.update') }}">
+                        @csrf
 
-    <link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-    <link rel="shortcut icon" href="/assets/media/logos/favicon.ico" />
-    <!--begin::Fonts-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
-    <!--end::Fonts-->
-    <!--begin::Global Stylesheets Bundle(used by all pages)-->
-    <link href="/assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
-    <link href="/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
-    <!--end::Global Stylesheets Bundle-->
-    <!--Begin::Google Tag Manager -->
-    <script>
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&amp;l='+l:'';j.async=true;j.src= '../../../../../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-5FS8GGP');
-    </script>
-    <!--End::Google Tag Manager -->
-</head>
-<!--end::Head-->
-<!--begin::Body-->
+                        <input type="hidden" name="token" value="{{ $token }}">
 
-<body id="kt_body" class="bg-body">
-    <!--Begin::Google Tag Manager (noscript) -->
-    <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5FS8GGP" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe>
-    </noscript>
-    <!--End::Google Tag Manager (noscript) -->
-    <!--begin::Main-->
-    <!--begin::Root-->
-    <div class="d-flex flex-column flex-root">
-        <!--begin::Authentication - Sign-up -->
-        <div class="d-flex flex-column flex-lg-row flex-column-fluid">
-            <!--begin::Aside-->
-            <div class="d-flex flex-column flex-lg-row-auto w-xl-600px positon-xl-relative"
-                style="background-color: #F2C98A">
-                <!--begin::Wrapper-->
-                <div class="d-flex flex-column position-xl-fixed top-0 bottom-0 w-xl-600px scroll-y">
-                    <!--begin::Content-->
-                    <div class="d-flex flex-row-fluid flex-column text-center p-10 pt-lg-20">
-                        <a href="/" class="mb-7">
-                            <img src="{{ asset('assets/img/logo/vtulogo.png') }}" srcset="{{ asset('assets/img/logo/vtulogo.png') }}"
-                              width='140px' height='35px' alt="">
-                
-                            {{-- <img alt="Logo" src="/assets/media/logos/ct_white.png" style='width:150px;height:40px' /> --}}
-                          </a>
-                          <!--end::Logo-->
-                
-                          <!--begin::Title-->
-                          <h2 class="text-white fw-normal m-0">
-                            Reset Your Password
-                          </h2>
-                    </div>
-                    <!--end::Content-->
-                    <!--begin::Illustration-->
-                    <div class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-350px"
-                        style="background-image: url(/metronic8/demo4/assets/media/illustrations/dozzy-1/13.png"></div>
-                    <!--end::Illustration-->
-                </div>
-                <!--end::Wrapper-->
-            </div>
-            <!--end::Aside-->
-            <!--begin::Body-->
-            <div class="d-flex flex-column flex-lg-row-fluid py-10">
-                <!--begin::Content-->
-                <div class="d-flex flex-center flex-column flex-column-fluid">
-                    <!--begin::Wrapper-->
-                    <div class="w-lg-600px p-10 p-lg-15 mx-auto">
-                      
-                     
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
 
-                        <form method="POST" action="{{ route('password.update') }}">
-                            @csrf
-                            <h1 class="text-dark mb-3">Set New Password</h1>
-                            <!--end::Title-->
-                            <!--begin::Link-->
-                            @if($errors->any())
-                            <div class="alert alert-danger">
-                                <p><strong>Opps Something went wrong</strong></p>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
-                            @endif
-                            <!-- Password Reset Token -->
-                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
-                
-                            <!-- Email Address -->
-                            <div>
-                                <x-label for="email" class='form-label fs-6 fw-bolder text-dark' :value="__('Email')" />
-                
-                                <x-input readonly id="email" class="block mt-1 w-full form-control" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
-                
-                            <!-- Password -->
-                            <div class="mt-4">
-                                <x-label for="password" class='form-label fs-6 fw-bolder text-dark' :value="__('Password')" />
-                
-                                <x-input id="password" class="block mt-1 w-full form-control" type="password" name="password" required />
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
-                
-                            <!-- Confirm Password -->
-                            <div class="mt-4">
-                                <x-label for="password_confirmation" class='form-label fs-6 fw-bolder text-dark' :value="__('Confirm Password')" />
-                
-                                <x-input id="password_confirmation" class="block mt-1 w-full form-control"
-                                                    type="password"
-                                                    name="password_confirmation" required />
-                            </div>
-                
-                            <div class="flex items-center justify-end mt-4">
-                                <x-button>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
                                     {{ __('Reset Password') }}
-                                </x-button>
+                                </button>
                             </div>
-                        </form>
-                        <!--end::Form-->
-                    </div>
-                    <!--end::Wrapper-->
+                        </div>
+                    </form>
                 </div>
-                <!--end::Content-->
-                <!--begin::Footer-->
-                <div class="d-flex flex-center flex-wrap fs-6 p-5 pb-0">
-                    <!--begin::Links-->
-                    <div class="d-flex flex-center fw-bold fs-6">
-                        <a href="https://cthostel.com" class="text-muted text-hover-primary px-2"
-                            target="_blank">CTHostel</a>
-                        <a href="/invest" class="text-muted text-hover-primary px-2" target="_blank">Invest</a>
-                        <a href="/invest" class="text-muted text-hover-primary px-2" target="_blank">Partner</a>
-                    </div>
-                    <!--end::Links-->
-                </div>
-                <!--end::Footer-->
             </div>
-            <!--end::Body-->
         </div>
-        <!--end::Authentication - Sign-up-->
     </div>
-    <!--end::Root-->
-    <!--end::Main-->
-    <!--begin::Javascript-->
-    <script>
-        var hostUrl = "/assets/index.html";
-    </script>
-    <!--begin::Global Javascript Bundle(used by all pages)-->
-    <script src="/assets/plugins/global/plugins.bundle.js"></script>
-    <script src="/assets/js/scripts.bundle.js"></script>
-    <!--end::Global Javascript Bundle-->
-    <!--begin::Page Custom Javascript(used by this page)-->
-    <script src="/assets/js/custom/authentication/sign-up/general.js"></script>
-    <script src='/assets/js/professionallocker.js'></script>
-
-    <!--end::Page Custom Javascript-->
-    <!--end::Javascript-->
-</body>
-<!--end::Body-->
-
-<!-- Mirrored from preview.keenthemes.com/metronic8/demo4/authentication/layouts/aside/sign-up.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 01 Feb 2022 02:20:44 GMT -->
-
-</html>
+</div>
+@endsection
