6d3353969caa0bd5283b06a8b2b86906315daf62
Femscode <fasanyafemi@gmail.com>
2023-11-29 03:52:25 +0100

    req_email

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -90,7 +90,7 @@
                                 <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
     
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{  $request->route('email') ?? old('email') }}" required autocomplete="email" autofocus>
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{  request('email') ?? old('email') }}" required autocomplete="email" autofocus>
     
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
