8441e49eb557f8bca4da6c041df439de8040b7e0
Femscode <fasanyafemi@gmail.com>
2023-12-06 09:43:19 +0100

    pay_ww_cc

--- app/Http/Controllers/FundingController.php
+++ app/Http/Controllers/FundingController.php
@@ -44,7 +44,7 @@ class FundingController extends Controller
         $data['amount'] = $amount = $request->amount;
         $data['active'] = 'fundwallet';
         if ($request->type == 'card') {
-            $env = User::where('email', 'fasanyafemi@gmail.com')->first()->twitter;
+            $env = env('FLW_SECRET_KEY');
 
             $data['public_key'] = $env;
             $data['callback_url'] = 'https://vtubiz.com/payment/callback';
@@ -107,7 +107,7 @@ class FundingController extends Controller
         $data['amount'] = $amount = $request->amount;
         $data['active'] = 'fundwallet';
         if ($request->type == 'card') {
-            $env = User::where('email', 'fasanyafemi@gmail.com')->first()->twitter;
+            $env = env('FLW_SECRET_KEY');
 
             $data['public_key'] = $env;
             // $data['public_key'] = env('FLW_PUBLIC_KEY');
