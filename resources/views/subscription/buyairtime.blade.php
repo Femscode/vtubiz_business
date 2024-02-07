30977fb12282d9b20b61f9bca64477a3441d76e1
Femscode <fasanyafemi@gmail.com>
2024-02-07 03:36:57 +0100

    new des

--- resources/views/subscription/buyairtime.blade.php
+++ resources/views/subscription/buyairtime.blade.php
@@ -15,8 +15,8 @@
             <!--begin::Content-->
             <div class='col-md-12'>
                 <div class='card '>
-                    <div class='alert alert-info'>
-                        <p>Do you know that you can buy airtime in bulk, create custom recipient groups (like staff or
+                    <div class='alert alert-primary'>
+                        <p>You can now buy airtime in bulk, create custom recipient groups (like staff or
                             family), and purchase airtime for every group member at once with a single click? Click <a
                                 style='color:red' href='/airtime_group'>here to try it out!</a></p>
                     </div>
