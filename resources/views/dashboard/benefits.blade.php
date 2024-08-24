070de5105a33e4115cc08ff1a3b2a0040d26c1be
Pelumi <fasanyafemi@gmail.com>
2024-08-24 09:23:07 +0100

    current level

--- resources/views/dashboard/benefits.blade.php
+++ resources/views/dashboard/benefits.blade.php
@@ -16,73 +16,98 @@
 
                 <!--begin::Misc Widget 1-->
 
-                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black' class='alert alert-dark'>
-                    <h4>Starter</h4>
-                    <p>Requirement : Registration</p>
-                    <ul>
-                        <li>Free access to all data plans.</li>
-                    </ul>
+                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black' @if($user->total_spent < 10000) class='alert alert-warning' @else class='alert alert-dark' @endif>
+                        <h4>Starter</h4>
+                        <p>Requirement : Registration</p>
+                        <ul>
+                            <li>Free access to all data plans.</li>
+                        </ul>
                 </div>
-                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black' class='alert alert-dark'>
-                    <h4>Enthusiast</h4>
-                    <p>Requirement : Perform transactions worth N10,000 and above.</p>
-                    <ul>
-                        <li>Free access to all data plans & VTU products.</li>
-                        <li>Schedule Purchase.</li>
-                        <li>Bulk Purchase.</li>
-                    </ul>
+                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black'
+                    @if($user->total_spent >= 10000 && $user->total_spent < 50000)
+                        class='alert alert-warning'
+                        @else
+                        class='alert alert-dark'
+                        @endif>
+                        <h4>Enthusiast</h4>
+                        <p>Requirement : Perform transactions worth N10,000 and above</p>
+                        <ul>
+                            <li>Free access to all data plans</li>
+                            <li>Schedule Purchase</li>
+                            <li>Bulk Purchase</li>
+                        </ul>
                 </div>
-                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black' class='alert alert-dark'>
-                    <h4>Loyal</h4>
-                    <p>Requirement : Perform transactions worth N50,000 and above.</p>
-                    <ul>
-                        <li>Free access to all data plans & VTU products.</li>
-                        <li>Schedule Purchase.</li>
-                        <li>Bulk Purchase.</li>
-                        <li>Ability to create giveaways.</li>
-                        <li>Free data worth <b>50MB</b> every <b>Tuesday</b>.</li>
-                    </ul>
-                </div>
-
-                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black' class='alert alert-dark'>
-                    <h4>Pro</h4>
-                    <p>Requirement : Perform transactions worth N200,000 and above.</p>
-                    <ul>
-                        <li>Free access to all data plans & VTU products.</li>
-                        <li>Schedule Purchase.</li>
-                        <li>Bulk Purchase.</li>
-                        <li>Ability to create and partake in giveaways.</li>
-                        <li>Free data worth <b>100MB</b> every <b>Tuesday</b>.</li>
-                    </ul>
+                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black'
+                    @if($user->total_spent >= 50000 && $user->total_spent < 200000)
+                        class='alert alert-warning'
+                        @else
+                        class='alert alert-dark'
+                        @endif>
+                        <h4>Loyal</h4>
+                        <p>Requirement : Perform transactions worth N50,000 and above</p>
+                        <ul>
+                            <li>Free access to all data plans</li>
+                            <li>Schedule Purchase</li>
+                            <li>Bulk Purchase</li>
+                            <li>Ability to create giveaways</li>
+                            <li>Free data worth 50MB weekly</li>
+                        </ul>
                 </div>
 
-                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black' class='alert alert-dark'>
-                    <h4>Elite</h4>
-                    <p>Requirement : Perform transactions worth N500,000 and above.</p>
-                    <ul>
-                        <li>Free access to all data plans & VTU products.</li>
-                        <li>Schedule Purchase.</li>
-                        <li>Bulk Purchase.</li>
-                        <li>Ability to create and partake in giveaways.</li>
-                        <li>Free data worth <b>500MB</b> every <b>Tuesday</b>.</li>
-                        <li>Get all products at a reseller price.</li>
-                        <li>A free VTU Website.</li>
-                        <li>Exclusive customer support.</li>
-                    </ul>
+                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black'
+                    @if($user->total_spent >= 200000 && $user->total_spent < 1000000)
+                        class='alert alert-warning'
+                        @else
+                        class='alert alert-dark'
+                        @endif>
+                        <h4>Pro</h4>
+                        <p>Requirement : Perform transactions worth N200,000 and above</p>
+                        <ul>
+                            <li>Free access to all data plans</li>
+                            <li>Schedule Purchase</li>
+                            <li>Bulk Purchase</li>
+                            <li>Ability to create and partake in giveaways</li>
+                            <li>Free data worth 100MB weekly</li>
+                            <li>Get all products at a reseller price</li>
+                        </ul>
                 </div>
-                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black' class='alert alert-dark'>
+                <div style='font-size:17px; font-weight:300; border-top:10px solid #856404;border:1px dashed black'
+                    @if($user->total_spent >= 1000000 && $user->total_spent < 5000000)
+                        class='alert alert-warning'
+                        @else
+                        class='alert alert-dark'
+                        @endif>
+                        <h4>Elite</h4>
+                        <p>Requirement : Perform transactions worth N500,000 and above</p>
+                        <ul>
+                            <li>Free access to all data plans</li>
+                            <li>Schedule Purchase</li>
+                            <li>Bulk Purchase</li>
+                            <li>Ability to create and partake in giveaways</li>
+                            <li>Free data worth 200MB weekly</li>
+                            <li>Get all products at a reseller price</li>
+                            <li>A free VTU Website</li>
+                        </ul>
+                </div>
+
+                <div style='font-size:17px; font-weight:300; border-top:10px solid #d4af37;border:1px dashed black'
+                    @if($user->total_spent >= 5000000)
+                    class='alert alert-warning'
+                    @else
+                    class='alert alert-dark'
+                    @endif>
                     <h4>Legend</h4>
-                    <p>Requirement : Perform transactions worth N1,000,000 and above.</p>
+                    <p>Requirement : Perform transactions worth N1,000,000 and above</p>
                     <ul>
-                        <li>Free access to all data plans & VTU products.</li>
-                        <li>Schedule Purchase.</li>
-                        <li>Bulk Purchase.</li>
-                        <li>Ability to create and partake in giveaways.</li>
-                        <li>Free data worth <b>1GB</b> every <b>Tuesday</b>.</li>
-                        <li>Get all products at a reseller price.</li>
-                        <li>A free VTU Website.</li>
-                        <li>Exclusive customer support.</li>
-                        <li>Priority access to new features and services.</li>
+                        <li>Free access to all data plans</li>
+                        <li>Schedule Purchase</li>
+                        <li>Bulk Purchase</li>
+                        <li>Ability to create and partake in giveaways</li>
+                        <li>Free data worth 500MB weekly</li>
+                        <li>Get all products at a reseller price</li>
+                        <li>A free VTU Website</li>
+                        <li>Exclusive customer support</li>
+                        <li>Priority access to new features and services</li>
                     </ul>
                 </div>
 
