245cd20e6cd515a5d6253376f22a574fd0cccfaf
Pelumi <fasanyafemi@gmail.com>
2025-05-27 17:57:54 +0100

    resolve pending

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -15,6 +15,8 @@ class WhatsappController extends Controller
     {
         //for transactions that are more than 2minuetes
         // $duplicate = DuplicateTransaction::where('created_at', '<=', now()->subMinutes(2))->first();
+        $duplicate =DuplicateTransaction::latest()->first();
+        dd($duplicate);
         $duplicate = DuplicateTransaction::where('created_at', '<', now()->subHour()->subMinutes(20))->first();
         $unresolved_transactions = Transaction::where('reference', 'like', 'data_p%')
             ->where('refund_status', 0)
