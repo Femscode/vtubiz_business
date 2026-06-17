9fb01c8fd81ff7a073a1ebd3e7241708baa8a32a
Pelumi <fasanyafemi@gmail.com>
2026-06-17 09:25:45 +0100

    ads removal

--- resources/views/dashboard/master1.blade.php
+++ resources/views/dashboard/master1.blade.php
@@ -449,16 +449,16 @@
             margin-bottom: var(--space-xl);
         }
     </style>
-    <script async custom-element="amp-auto-ads"
+    <!-- <script async custom-element="amp-auto-ads"
         src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
-    </script>
+    </script> -->
     @yield('header')
 </head>
 
 <body>
-    <amp-auto-ads type="adsense"
+    <!-- <amp-auto-ads type="adsense"
         data-ad-client="ca-pub-9520357947525167">
-    </amp-auto-ads>
+    </amp-auto-ads> -->
     <div id="app" class="dashboard-container">
         <!-- Sidebar Overlay -->
         <div class="sidebar-overlay" onclick="toggleSidebar()"></div>
